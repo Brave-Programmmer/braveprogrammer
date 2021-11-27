7b792d126f999dece41351db1cb3132a7ca8abc1
Sandeep <spujari79@gmail.com>
2021-11-27 12:16:48 +0530

    Navbar and Login Nav

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask,render_template
-
+from flask import Flask, render_template
+import pymongo
 app = Flask(__name__)
+
+client = pymongo.MongoClient('mongodb://localhost:27017/')
+db = client.pro_programmer
+collection = db.user
 
 @app.route("/")
 def home():
     return render_template('index.html')
 
+@app.route("/login")
+def login():
+    return render_template('login.html')
+
+@app.route("/signup")
+def signup():
+    return render_template('signup.html')
 if __name__ == '__main__':
     app.run(debug=True)
