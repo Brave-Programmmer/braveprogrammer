e52d719b2a2e545941d90f0f27beb466dbaeb8e2
Sandeep <spujari79@gmail.com>
2021-12-05 10:02:49 +0530

    All Blog Rendering Done!!!😀

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask, render_template, request
 import pymongo
-app = Flask(__name__)
 
+
+app = Flask(__name__)
 client = pymongo.MongoClient('mongodb://localhost:27017/')
 db = client.pro_programmer
 collection = db.Blog
 
-
+# Routes
 @app.route("/")
 def home():
     return render_template('index.html')
@@ -14,9 +15,6 @@ def home():
 
 @app.route('/search', methods=['GET', 'POST'])
 def onsearch():
-    # print(request.form.get('username'))
-    # print(request.method)
-    # print(request.form.get('searchtext'))
     if request.method == 'POST':
         find = db.Blog.find(
             {"title": {"$regex": request.form.get('searchtext')}})
@@ -30,7 +28,7 @@ def onsearch():
 
 
 @app.route("/blog/<string:slug>",methods=['GET'])
-def allblog(slug):
+def blog(slug):
     print(request.method)
     if request.method == 'GET':
         query = db.Blog.find({"slug":slug})
@@ -41,7 +39,19 @@ def allblog(slug):
     else:
         print('Invaild Request')
         
+@app.route("/blog",methods=['GET'])
+def allblog():
+    print(request.method)
+    if request.method == 'GET':
+       findallblog = db.Blog.find().limit(4)
+       if findallblog:
+           return render_template('search.html', noblog=False, blog=findallblog)
+       else:
+           return render_template('search.html', noblog=True)
+            
 
-
+    else:
+        print('Invaild Request')
+# Runing app
 if __name__ == '__main__':
     app.run(debug=True)
