6a0995e7e8b161d14e6ca9e8688cde00353c6d13
sandeepdp <86524672+sandeepdp@users.noreply.github.com>
2021-12-08 17:58:49 +0530

    Pagination Work

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, Markup
-
+import math
 import pymongo
 
 
@@ -47,42 +47,17 @@ def blog(slug):
 @app.route("/blog", methods=['GET'])
 def allblog():
     if request.method == 'GET':
-        from_arg = int(request.args.get('from'))
-        to_arg = int(request.args.get('to'))
-        if str(from_arg).isnumeric() and str(to_arg).isnumeric():
-            last = db.Blog.count_documents({})
-            findalblog = db.Blog.find().skip(int(to_arg)).limit(int(from_arg))
-            if findalblog:
-                # next_url = int(req1) + 4
-                # pre_url = int(req2) + 4
-                # next_url1 = int(req1) - 4
-                # pre_url1 = int(req2) - 4
-                # return render_template('search.html', noblog=False,Markup=Markup,num = num, blog=findallblog, next_url=next_url, pre_url = pre_url, next_url1=next_url1, pre_url1 = pre_url1)
-                #First Page
-                if from_arg == 0:
-                    pre_url = '#'
-                    next_url = 'from=' + str(int(from_arg) + 4) + '&to=' + str(int(to_arg) + 4)
-                    print(next_url)
-                    return render_template('search.html', noblog=False, Markup=Markup, next_url=next_url, pre_url=pre_url)
-                # Last Page
-                elif to_arg == last:
-                    next_url = '#'
-                    pre_url = 'from=' + str(int(from_arg) +- 4) + '&to=' + str(int(to_arg) - 4)
-                    print(next_url)
-                    return render_template('search.html', noblog=False, Markup=Markup, next_url=next_url, pre_url=pre_url)
-                #Middle Page
-                else:
-                    next_url = 'from=' + str(int(from_arg) + 4) + '&to=' + str(int(to_arg) + 4)
-                    pre_url = 'from=' + str(int(from_arg) - 4) + '&to=' + str(int(to_arg) - 4)
-
-            else:
-                return render_template('search.html', noblog=True)
+        pageno = int(request.args.get('page'))#Page no 3
+        noblogs = db.Blog.count_documents({})#no of blogs 16
+        nopages = math.ceil(pageno/noblogs)#no of pages 4
+        print(nopages)
+        findallblog = db.Blog.find({}).skip((pageno * pageno)-1).limit(4)
+        for doc in findallblog:
+            print(doc)
+        if pageno <= nopages:
+            return render_template('search.html')
         else:
-            return print('404')
-
-    else:
-        return print('Invaild Request')
-
+            return print('no page')
 
 # Runing app
 if __name__ == '__main__':
