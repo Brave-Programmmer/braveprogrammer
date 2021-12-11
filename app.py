4ee174f7df34efb9d1cf0cf2287c8fa3268870fe
Sandeep <spujari79@gmail.com>
2021-12-11 14:46:11 +0530

    Pagination Done !!! Happy😀

--- app.py
+++ app.py
@@ -47,15 +47,60 @@ def blog(slug):
 @app.route("/blog", methods=['GET'])
 def allblog():
     if request.method == 'GET':
-        pageno = int(request.args.get('page'))#Page no 3
-        noblogs = db.Blog.count_documents({})#no of blogs 16
-        nopages = math.ceil(noblogs/4)#no of pages 4
+        pageno = int(request.args.get('page'))  # Current Page eg 2
+        noblogs = db.Blog.count_documents({})  # Total no of blogs 16
+        nopages = math.ceil(noblogs/4)  # no of pages can be made 4
         print(nopages)
-        findallblog = db.Blog.find({}).skip((pageno * pageno)).limit(4)
-        if pageno < nopages or pageno == nopages:
-            return render_template('search.html', allblog = findallblog, Markup= Markup)
+        # first page
+        if pageno == 1:
+            findallblog = db.Blog.find({}).skip((pageno * pageno) - 1).limit(4)
+            pre_url = "no"
+            next_url = f'/blog?page={pageno + 1}'
+            # print(next_url)
+            return render_template(
+                'search.html', 
+                allblog=findallblog,
+                Markup=Markup,
+                pageno=pageno,
+                pre_url = pre_url, 
+                next_url = next_url
+                   )
+        elif pageno == nopages:
+            findallblog = db.Blog.find({}).skip((pageno * pageno) - 1).limit(4)
+            pre_url = f'/blog?page={pageno - 1}'
+            next_url = "no"
+            # print(next_url)
+            return render_template(
+                'search.html', 
+                allblog=findallblog,
+                Markup=Markup,
+                pageno=pageno,
+                pre_url = pre_url, 
+                next_url = next_url
+                   )
         else:
-            return print('no page')
+            findallblog = db.Blog.find({}).skip((pageno * pageno) - 1).limit(4)
+            pre_url = f'/blog?page={pageno - 1}'
+            next_url = f'/blog?page={pageno + 1}'
+            # print(next_url)
+            return render_template(
+                'search.html', 
+                allblog=findallblog,
+                Markup=Markup,
+                pageno=pageno,
+                pre_url = pre_url, 
+                next_url = next_url
+                   )
+        #Last Page
+        # elif pageno == nopages:
+        #     findallblog = db.Blog.find({}).skip((pageno * pageno) - 1).limit(4)
+        #     return render_template('search.html', allblog=findallblog, Markup=Markup, pageno=pageno)
+        # findallblog = db.Blog.find({}).skip((pageno * pageno)).limit(4)
+        # if pageno < nopages or pageno == nopages:
+        #     return render_template('search.html', allblog=findallblog, Markup=Markup)
+        # else:
+        #     return print('no page')
+
 
 # Runing app
 if __name__ == '__main__':
